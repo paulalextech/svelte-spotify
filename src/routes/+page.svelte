4f2018af17678e0c2da1a3bd09bef5ea4944ce27
paulalextech <psteane123@hotmail.com>
2024-07-10 11:52:11 +0100

    set up sass setting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,15 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <style lang="scss">
-	@use '../styles/functions';
+	@use '@unsass/breakpoint';
 	h1 {
 		color: red;
 		font-size: functions.toRem(36);
+		@include breakpoint.up('md') {
+			color: greenyellow;
+		}
+		@include breakpoint.up(lg) {
+			color: purple;
+		}
 	}
 </style>
