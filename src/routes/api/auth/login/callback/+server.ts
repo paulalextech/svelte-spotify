b5fdcc0d66f043a1d3873648236fe16269a12272
paulalextech <psteane123@hotmail.com>
2024-07-15 14:30:55 +0100

    created url call

--- src/routes/api/auth/login/callback/+server.ts
+++ src/routes/api/auth/login/callback/+server.ts
@@ -1,8 +1,31 @@
+import { error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import { SPOTIFY_APP_CLIENT_ID, SPOTIFY_APP_CLIENT_SECRET } from "$env/static/private";
 
-export const GET: RequestHandler = ({ url, cookies }) => {
+
+export const GET: RequestHandler = ({ url, cookies, catch }) => {
 	const code = url.searchParams.get('code') || null;
 	const state = url.searchParams.get('state') || null;
 
 	const storedState = cookies.get('spotify_auth_state') || null;
+	const storedChallengeVerifier = cookies.get('spotify_auth_challenge_verifier') || null;
+
+	if (state === null || state !== storedState) {
+		throw error(400, 'State Mismatch');
+	}
+
+	const response = await fetch('https://accounts.spotify.com/api/tokens', {
+		method: "POST",
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			Authorization: `Basic ${Buffer.from(`${SPOTIFY_APP_CLIENT_ID }:${ SPOTIFY_APP_CLIENT_SECRET}`).toString('base64')}`
+		},
+		body: new URLSearchParams({
+			code: code || '',
+			redirect_url: `${BASE_URL}/api/auth/callback`,
+			grant_type: 'authorization_code',
+			code_verifier: storedChallengeVerifier || '',
+			client_id: SPOTIFY_APP_CLIENT_ID
+		})
+	})
 };
