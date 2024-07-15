315099bca27d2e3ad54c2dad2466809c98f682a3
paulalextech <psteane123@hotmail.com>
2024-07-15 10:34:38 +0100

    added scope

--- src/routes/api/auth/login/+server.ts
+++ src/routes/api/auth/login/+server.ts
@@ -1,4 +1,5 @@
 import { redirect, type RequestHandler } from '@sveltejs/kit';
+import pkce from 'pkce-gen';
 import { SPOTIFY_APP_CLIENT_ID, BASE_URL } from '$env/static/private';
 
 const generateRandomString = (length: number) => {
@@ -11,7 +12,11 @@ const generateRandomString = (length: number) => {
 	return randomString;
 };
 
-const scope = '';
+const state = generateRandomString(16);
+const challenge = pkce.create();
+
+const scope =
+	'ugc-image-upload user-modify-playback-state user-read-playback-state user-read-currently-playing user-follow-modify user-follow-read user-read-recently-played user-read-playback-position user-top-read playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private app-remote-control streaming user-read-email user-read-private user-library-modify user-library-read';
 
 export const GET: RequestHandler = () => {
 	throw redirect(
@@ -21,7 +26,9 @@ export const GET: RequestHandler = () => {
 			client_id: SPOTIFY_APP_CLIENT_ID,
 			scope,
 			redirect_uri: `${BASE_URL}/api/auth/callback`,
-			state: 
+			state,
+			code_challenge_method: 'S256',
+			code_challenge: challenge.code_challenge
 		})}`
 	);
 };
