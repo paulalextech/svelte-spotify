3f0daa8c3f1fb5e9fee60cf6d8f5d68263ee79ea
paulalextech <psteane123@hotmail.com>
2024-07-15 10:25:53 +0100

    random string maker

--- src/routes/api/auth/login/+server.ts
+++ src/routes/api/auth/login/+server.ts
@@ -1,5 +1,27 @@
 import { redirect, type RequestHandler } from '@sveltejs/kit';
+import { SPOTIFY_APP_CLIENT_ID, BASE_URL } from '$env/static/private';
+
+const generateRandomString = (length: number) => {
+	let randomString = '';
+	const possibleChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+	for (let i = 0; i < length; i++) {
+		randomString += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+	}
+	return randomString;
+};
+
+const scope = '';
 
 export const GET: RequestHandler = () => {
-	throw redirect(307, `https://accounts.spotify.com/authorize?`);
+	throw redirect(
+		307,
+		`https://accounts.spotify.com/authorize?${new URLSearchParams({
+			response_type: 'code',
+			client_id: SPOTIFY_APP_CLIENT_ID,
+			scope,
+			redirect_uri: `${BASE_URL}/api/auth/callback`,
+			state: 
+		})}`
+	);
 };
